d203428ef8bdbbb5aebe3df3674ae73aec20ffbc
jinxiong.hou <jinxiong.hou@aispeech.com>
2016-10-11 17:35:15 +0800

    unzip update

--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -2,7 +2,7 @@
 * @Author: jinggoing
 * @Date:   2016-10-11 10:42:52
 * @Last Modified by:   jinxiong.hou
-* @Last Modified time: 2016-10-11 17:26:47
+* @Last Modified time: 2016-10-11 17:33:58
 */
 
 var bodyParser = require('body-parser');
@@ -15,8 +15,8 @@ module.exports = function (app) {
 	app.route('/upload').post(indexController.upload);
 	app.route('/deleteFolder').get(indexController.deleteFolder);
 	//app.route('/readZip').get(indexController.readZip);
-/*	app.route('/unZip').get(indexController.unZip);
-	app.route('/readFolder').get(indexController.readFolder);*/
+	app.route('/unZip').get(indexController.unZip);
+/*	app.route('/readFolder').get(indexController.readFolder);*/
 
 	app.get('/', function(req,res){
         res.sendfile( './src/index.html');
